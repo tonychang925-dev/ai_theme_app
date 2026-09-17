a2fab19eff5d40e18f27e35a6368d838aadc5e32
tonychang925-dev <tonychang925@gmail.com>
2026-09-17 22:19:54 +0800

    feat(market): export canonical public result contracts

--- market_public/__init__.py
+++ market_public/__init__.py
@@ -1,10 +1,43 @@
 from .contracts import (
-    CAPABILITIES, CapabilityMetadata, EventReadRequest, EventResolveRequest,
-    MarketResult, MarketStatus, ProductReadRequest,
+    CAPABILITIES,
+    MARKET_BOUNDARY_IDENTITY_REF,
+    MARKET_PUBLIC_CONTRACT_VERSION,
+    CapabilityMetadata,
+    EventReadRequest,
+    EventResolveRequest,
+    MarketDataState,
+    MarketFailure,
+    MarketFailureKind,
+    MarketOperationStatus,
+    MarketResultEnvelope,
+    ProductReadRequest,
 )
 from .factory import MarketPublicFactory
+from .provenance import (
+    MarketProvenance,
+    MarketProvenanceProfile,
+    MarketProvenanceStatus,
+    MarketReleaseIdentity,
+    ProvenancePredicate,
+)
 
 __all__ = [
-    "CAPABILITIES", "CapabilityMetadata", "EventReadRequest", "EventResolveRequest",
-    "MarketResult", "MarketStatus", "ProductReadRequest", "MarketPublicFactory",
+    "CAPABILITIES",
+    "MARKET_BOUNDARY_IDENTITY_REF",
+    "MARKET_PUBLIC_CONTRACT_VERSION",
+    "CapabilityMetadata",
+    "EventReadRequest",
+    "EventResolveRequest",
+    "MarketDataState",
+    "MarketFailure",
+    "MarketFailureKind",
+    "MarketOperationStatus",
+    "MarketProvenance",
+    "MarketProvenanceProfile",
+    "MarketProvenanceStatus",
+    "MarketPublicFactory",
+    "MarketReleaseIdentity",
+    "MarketResultEnvelope",
+    "ProductReadRequest",
+    "ProvenancePredicate",
 ]
